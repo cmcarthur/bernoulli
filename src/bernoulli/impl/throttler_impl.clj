07bf3083fc5adbb0e508a51272dfb233c63859c3
Connor McArthur <cmcarthur@rjmetrics.com>
2014-12-05 16:06:42 -0500

    make  transactional

--- src/bernoulli/impl/throttler_impl.clj
+++ src/bernoulli/impl/throttler_impl.clj
@@ -1,6 +1,5 @@
 (ns bernoulli.impl.throttler-impl
-  (:require [com.climate.claypoole :as threadpool]
-            [clj-time.core :as time])
+  (:require [com.climate.claypoole :as threadpool])
   (:refer-clojure :exclude [delay]))
 
 (defn now
@@ -10,9 +9,11 @@
 (defn delay
   [this]
   (let [n (now)
-        time-since-last-call-ms (- n @(:last-call-time-ms this))
+        last-call-time-ms @(:last-call-time-ms this)
+        time-since-last-call-ms (- n last-call-time-ms)
         delay-time-ms (max 0 (- (:time-ms this) time-since-last-call-ms))]
-    (reset! (:last-call-time-ms this) (+ n delay-time-ms))
-    (when (not= 0 delay-time-ms)
-      (deref (threadpool/future (:pool this)
-                                (Thread/sleep delay-time-ms))))))
+    (if-not (compare-and-set! (:last-call-time-ms this) last-call-time-ms (+ n delay-time-ms))
+      (recur this)
+      (when (not= 0 delay-time-ms)
+        (deref (threadpool/future (:pool this)
+                                  (Thread/sleep delay-time-ms)))))))
